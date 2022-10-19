12bf0cdda5de8989468284d5f84d6288d6a44f4f
Liam Howley <liam.howley@hotmail.com>
2022-10-19 12:56:46 +0100

    unnecessary as print-slot in xml already covers this

--- print.lisp
+++ print.lisp
@@ -15,21 +15,6 @@
 	(print-slot slot-value definition (slot-definition-type slot-definition) stream)))))
 
 
-;; xml attributes cannot have multiple values. However, html attributes can:
-(defmethod print-slot ((object dom-node) (slot html-direct-slot-definition) (type (eql 'cons)) (stream stream))
-  ;; attribute name
-  (write-string " " stream)
-  (write-string (slot-definition-attribute slot) stream)
-  (write-string "='" stream)
-  ;; attribute values
-  (let ((slot-name (slot-definition-name slot)))
-    (let ((values (slot-value object slot-name)))
-      (loop for value in values
-	    do (write-string value stream)
-	    if (equal value (car (last values)))
-	      do (write-string "'" stream)
-	    else do (write-string " " stream)))))
-
 (defmethod print-slot ((object dom-node) (slot html-direct-slot-definition) (type (eql 'boolean)) (stream stream))
   (write-string " " stream)
   (write-string (slot-definition-attribute slot) stream))
