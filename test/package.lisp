304ec9d36d6015667c2ee6c0356b2be21b5f9cc7
Liam Howley <liam.howley@hotmail.com>
2022-12-21 02:11:05 +0000

    resolving conflicts

--- test/package.lisp
+++ test/package.lisp
@@ -1,5 +1,10 @@
 (defpackage html.test
   (:use :cl :parachute :xml.parse :html.parse)
+  (:shadowing-import-from
+   :parachute
+   :body
+   :output
+   :summary)
   (:export :test-parse))
 
 (in-package html.test)
